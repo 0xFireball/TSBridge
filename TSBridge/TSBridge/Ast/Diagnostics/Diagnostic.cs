70edd483d77a125e59812122b883974d5b2586bd
0xFireball <0xFireball@outlook.com>
2017-01-27 14:04:07 -0800

    support diagnostic parsing

---
+++ TSBridge/TSBridge/Ast/Diagnostics/Diagnostic.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace TSBridge.Ast.Diagnostics
+{
+    public class Diagnostic
+    {
+        public enum DiagnosticCategory
+        {
+            Warning = 0,
+            Error = 1,
+            Message = 2,
+        }
+
+        [JsonProperty("file")]
+        public object File { get; set; }
+
+        [JsonProperty("start")]
+        public int Start { get; set; }
+
+        [JsonProperty("length")]
+        public int Length { get; set; }
+
+        [JsonProperty("messageText")]
+        public string MessageText { get; set; }
+
+        [JsonProperty("category")]
+        public DiagnosticCategory Category { get; set; }
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+    }
+}
